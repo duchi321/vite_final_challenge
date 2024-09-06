23efa125e6651c5abc42a262a878e9f58ef56c0e
duchi321 <sqaz0502@gmail.com>
2024-09-06 22:55:01 +0800

    Reset routing.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <RouterLink to="/signup">註冊</RouterLink> / <RouterLink to="/login">登入</RouterLink> /
-    <RouterLink to="todo">Todo</RouterLink>
-  </div>
   <RouterView />
 </template>
 
